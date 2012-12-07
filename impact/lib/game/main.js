0add1362339faa9084342feeb99ed0fce14d0533
Aaron Kolker <akolker2@me.com>
2012-12-07 16:32:27 -0500

    This is the first commit

---
+++ impact/lib/game/main.js
@@ -0,0 +1,61 @@
+ig.module( 
+	'game.main' 
+)
+.requires(
+	'impact.game',
+	'impact.font',
+	'game.entities.player',
+	'game.levels.test',
+	'impact.debug.debug'
+)
+.defines(function(){
+
+MyGame = ig.Game.extend({
+	gravity: 0,
+
+	// Load a font
+	font: new ig.Font( 'media/04b03.font.png' ),
+	
+	
+	init: function() {
+		// Initialize your game here; bind keys etc.
+		ig.input.bind( ig.KEY.LEFT_ARROW, 'left' );
+		ig.input.bind( ig.KEY.RIGHT_ARROW, 'right' );
+		ig.input.bind( ig.KEY.UP_ARROW, 'forward');
+		ig.input.bind( ig.KEY.DOWN_ARROW, 'backwards');
+		ig.input.bind( ig.KEY.C, 'shoot' );
+		this.loadLevel(LevelTest);
+	},
+	
+	update: function() {
+		// Update all entities and backgroundMaps
+		this.parent();
+		
+		// Add your own, additional update code here
+	},
+	
+	draw: function() {
+		//moving camera
+		var player = this.getEntitiesByType( EntityPlayer )[0];
+		if( player ) {
+			this.screen.x = player.pos.x - ig.system.width/2;
+			this.screen.y = player.pos.y - ig.system.height/2;
+		}
+
+		// Draw all entities and backgroundMaps
+		this.parent();
+		
+		
+		// Add your own drawing code here
+		var x = ig.system.width/2,
+			y = ig.system.height/2;
+		
+		}
+});
+
+
+// Start the Game with 60fps, a resolution of 320x240, scaled
+// up by a factor of 2
+ig.main( '#canvas', MyGame, 60, 400, 290, 2 );
+
+});
